e2c8fb54d7910d2aae9ee3f33e6a5e4fbe41daf7
Harry <gurikin@yandex.ru>
2020-08-18 23:30:16 +0300

    02_product_list fixed getPizzas route. Added getToppings route

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Product;
@@ -11,9 +12,18 @@ class IndexController extends AbstractController
     /**
      * @Route("/api/v1/getPizzas", name="get_pizza_list")
      */
-    public function index()
+    public function getPizzas()
     {
-      $pizzaList = $this->getDoctrine()->getRepository(Product::class)->findAll();
+      $pizzaList = $this->getDoctrine()->getRepository(Product::class)->findBy(['productType' => ProductType::PIZZA_TYPE]);
       return $this->json($pizzaList);
+    }
+
+    /**
+     * @Route("/api/v1/getToppings", name="get_topping_list")
+     */
+    public function getToppings()
+    {
+        $toppingList = $this->getDoctrine()->getRepository(Product::class)->findBy(['productType' => ProductType::TOPPING_TYPE]);
+        return $this->json($toppingList);
     }
 }
