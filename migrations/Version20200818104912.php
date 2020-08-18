ed134f9c35c3621a45f6759dba19478c2dc04e45
ibanchikov <ibanchikov@direct-credit.ru>
2020-08-18 13:56:38 +0300

    master init project

---
+++ migrations/Version20200818104912.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200818104912 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'create product_type table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $sql = 'CREATE TABLE if not exists `product_types` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `name` varchar(255) NOT NULL,
+            PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci';
+        $this->addSql($sql);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $sql = 'DROP TABLE IF EXISTS `product_types`';
+        $this->addSql($sql);
+    }
+}
