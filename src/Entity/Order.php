8a839cc3a7a93486a57e1cded686ebc300042ec9
ibanchikov <ibanchikov@direct-credit.ru>
2020-08-25 14:34:58 +0300

    05_order prepared for order functional

---
+++ src/Entity/Order.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrderRepository::class)
+ * @ORM\Table(name="`order`")
+ */
+class Order
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $surname;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $productsCost;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $deliveryCost;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(?string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getProductsCost(): ?float
+    {
+        return $this->productsCost;
+    }
+
+    public function setProductsCost(float $productsCost): self
+    {
+        $this->productsCost = $productsCost;
+
+        return $this;
+    }
+
+    public function getDeliveryCost(): ?float
+    {
+        return $this->deliveryCost;
+    }
+
+    public function setDeliveryCost(float $deliveryCost): self
+    {
+        $this->deliveryCost = $deliveryCost;
+
+        return $this;
+    }
+}
