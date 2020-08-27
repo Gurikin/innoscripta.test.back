ed7ff7aa5cfbab19ecac76c6851261a0bc117bc4
ibanchikov <ibanchikov@direct-credit.ru>
2020-08-27 14:23:59 +0300

    move to pgsql for the heroku

---
+++ migrations/Version20200827110550.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200827110550 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE cart_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE cart_product_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE customer_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE "order_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE cart (id INT NOT NULL, total_price DOUBLE PRECISION NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE cart_product (id INT NOT NULL, cart_id INT NOT NULL, product_id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_2890CCAA1AD5CDBF ON cart_product (cart_id)');
+        $this->addSql('CREATE INDEX IDX_2890CCAA4584665A ON cart_product (product_id)');
+        $this->addSql('CREATE TABLE customer (id INT NOT NULL, user_id INT DEFAULT NULL, cart_id INT DEFAULT NULL, token VARCHAR(255) NOT NULL, token_expired_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_81398E09A76ED395 ON customer (user_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_81398E091AD5CDBF ON customer (cart_id)');
+        $this->addSql('CREATE TABLE "order" (id INT NOT NULL, customer_id INT NOT NULL, name VARCHAR(255) NOT NULL, surname VARCHAR(255) DEFAULT NULL, address TEXT NOT NULL, phone VARCHAR(255) NOT NULL, products_cost DOUBLE PRECISION NOT NULL, delivery_cost DOUBLE PRECISION NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_F52993989395C3F3 ON "order" (customer_id)');
+        $this->addSql('CREATE TABLE product (id SERIAL NOT NULL, product_type INT DEFAULT NULL, name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, description TEXT DEFAULT NULL, image_url VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX product_type ON product (product_type)');
+        $this->addSql('CREATE UNIQUE INDEX name ON product (name, product_type)');
+        $this->addSql('CREATE TABLE product_types (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+        $this->addSql('ALTER TABLE cart_product ADD CONSTRAINT FK_2890CCAA1AD5CDBF FOREIGN KEY (cart_id) REFERENCES cart (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE cart_product ADD CONSTRAINT FK_2890CCAA4584665A FOREIGN KEY (product_id) REFERENCES product (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE customer ADD CONSTRAINT FK_81398E09A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE customer ADD CONSTRAINT FK_81398E091AD5CDBF FOREIGN KEY (cart_id) REFERENCES cart (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE "order" ADD CONSTRAINT FK_F52993989395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD1367588 FOREIGN KEY (product_type) REFERENCES product_types (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA pd');
+        $this->addSql('ALTER TABLE cart_product DROP CONSTRAINT FK_2890CCAA1AD5CDBF');
+        $this->addSql('ALTER TABLE customer DROP CONSTRAINT FK_81398E091AD5CDBF');
+        $this->addSql('ALTER TABLE "order" DROP CONSTRAINT FK_F52993989395C3F3');
+        $this->addSql('ALTER TABLE cart_product DROP CONSTRAINT FK_2890CCAA4584665A');
+        $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04AD1367588');
+        $this->addSql('ALTER TABLE customer DROP CONSTRAINT FK_81398E09A76ED395');
+        $this->addSql('DROP SEQUENCE cart_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE cart_product_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE customer_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE "order_id_seq" CASCADE');
+        $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
+        $this->addSql('DROP TABLE cart');
+        $this->addSql('DROP TABLE cart_product');
+        $this->addSql('DROP TABLE customer');
+        $this->addSql('DROP TABLE "order"');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE product_types');
+        $this->addSql('DROP TABLE "user"');
+    }
+}
