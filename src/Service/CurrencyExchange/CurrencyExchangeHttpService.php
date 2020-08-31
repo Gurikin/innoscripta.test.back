73590a2716037a324c4e7955c2118fb20178211c
Harry <gurikin@yandex.ru>
2020-08-31 07:15:38 +0300

    add cache for the currency exchange

--- src/Service/CurrencyExchange/CurrencyExchangeHttpService.php
+++ src/Service/CurrencyExchange/CurrencyExchangeHttpService.php
@@ -4,7 +4,8 @@
 namespace App\Service\CurrencyExchange;
 
 
-use stdClass;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\CacheItem;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
@@ -14,11 +15,16 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class CurrencyExchangeHttpService implements CurrencyExchangeInterface
 {
     private const EXCHANGE_API_URL = 'https://api.exchangeratesapi.io/latest';
+    private const CURRENCY_EXCHANGE_PREFIX = 'currency.exchange';
+    private const CURRENCY_RATE_REFRESH_TIME = 24*60*60;
 
     /**
      * @var HttpClientInterface
      */
     private HttpClientInterface $httpClient;
+    /** @var FilesystemAdapter $cache */
+    private $cache;
+
 
     /**
      * TwigExtinsion constructor.
@@ -27,22 +33,32 @@ class CurrencyExchangeHttpService implements CurrencyExchangeInterface
     public function __construct(HttpClientInterface $httpClient)
     {
         $this->httpClient = $httpClient;
+        $this->cache = new FilesystemAdapter();
     }
 
 
     /**
      * @inheritDoc
      * @throws TransportExceptionInterface
+     * @throws \Psr\Cache\InvalidArgumentException
      */
     public function getRate(string $toCurrency): float
     {
+        /** @var CacheItem $rate */
+        $rate = $this->cache->getItem(self::CURRENCY_EXCHANGE_PREFIX . '.' . $toCurrency);
+        if ($rate->get()) {
+            return $rate->get();
+        }
         $rates = $this->httpClient->request(
             'GET',
             self::EXCHANGE_API_URL
         );
         try {
             $ratesObject = json_decode($rates->getContent());
-            return $ratesObject->rates->$toCurrency ? $ratesObject->rates->$toCurrency : 1.0;
+            $rate->set($ratesObject->rates->$toCurrency ? $ratesObject->rates->$toCurrency : 1.0);
+            $rate->expiresAfter(self::CURRENCY_RATE_REFRESH_TIME);
+            $this->cache->save($rate);
+            return $rate->get();
         } catch (ClientExceptionInterface $e) {
         } catch (RedirectionExceptionInterface $e) {
         } catch (ServerExceptionInterface $e) {
